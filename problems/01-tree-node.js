bf3c6eb5a1242d9911f0e0baddd534344b418f71
Kevin Mei <kevjinx@gmail.com>
2021-05-24 14:56:22 -0700

    preorderarray

--- problems/01-tree-node.js
+++ problems/01-tree-node.js
@@ -3,6 +3,34 @@
 // and constructs a valid binary tree node
 class TreeNode {
     // Your code here
+  constructor(val) {
+    this.val = val;
+    this.left = null
+    this.right = null
+  }
+
+  preOrderArray(root) {
+    if (!root.left && !root.right) {
+      return root
+    } else {
+      if (root.val <= this.left.val) {
+        return this.preOrderArray(root.left)
+      } else {
+        return this.preOrderArray(root.right)
+      }
+    }
+  }
+
+
+  inOrderArray() {
+
+  }
+
+  postOrderArray() {
+
+  }
+
+
 }
 
 module.exports = {
