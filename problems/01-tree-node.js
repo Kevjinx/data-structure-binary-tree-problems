2f662cfd39a7daa3bebc02749dc9387a359111d3
Kevin Mei <kevjinx@gmail.com>
2021-05-24 15:03:01 -0700

    preorderarray finish

--- problems/01-tree-node.js
+++ problems/01-tree-node.js
@@ -1,5 +1,8 @@
 
 // Implement a class that takes a value
+
+const { preOrderArray } = require("./02-tree-order");
+
 // and constructs a valid binary tree node
 class TreeNode {
     // Your code here
@@ -9,17 +12,34 @@ class TreeNode {
     this.right = null
   }
 
-  preOrderArray(root) {
-    if (!root.left && !root.right) {
-      return root
-    } else {
-      if (root.val <= this.left.val) {
-        return this.preOrderArray(root.left)
-      } else {
-        return this.preOrderArray(root.right)
-      }
-    }
+  // preOrderArray(root) {
+  //   if (!root.left && !root.right) {
+  //     return root
+  //   } else {
+  //     if (root.val <= this.left.val) {
+  //       return this.preOrderArray(root.left)
+  //     } else {
+  //       return this.preOrderArray(root.right)
+  //     }
+  //   }
+  // }
+
+
+//if have left,
+//  go left
+//else if have right
+// go right
+//else return root
+
+
+  postOrderArray(node) {
+    if (!node.left && !node.right) return node;
+    if (node.left) return preOrderArray(node.left)
+    if (node.right) return preOrderArray(node.right)
   }
+
+
+
 
 
   inOrderArray() {
